70c7fadebf1b5002b6101ee99f179f7d3eef0b79
Nathan LeClaire <nathan.leclaire@gmail.com>
2013-11-14 20:02:38 -0500

    a few minor semantic things but most notably a successful ajax call

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,18 @@ angular.module('myApp.controllers', []).
   controller('MainCtrl', function($scope, $http) {
   	$http.defaults.useXDomain = true;
   	$scope.runningTest = false;
-  	$scope.done_parsing_urls = false;
+  	$scope.done_scraping_original_url = false;
   	$scope.startRunningTest = function() {
   		$scope.runningTest = true;
-  		console.log($scope.url_to_parse);
+  		console.log($scope.url_to_scrape);
+  		$http.get('/slurp', {
+			params: {
+				url_to_scrape: $scope.url_to_scrape
+			}
+  		})
+  		.success(function(data) {
+  			$scope.done_scraping_original_url = true;
+  			$scope.externalWebSiteHTML = data;
+  		});
   	};
   });
