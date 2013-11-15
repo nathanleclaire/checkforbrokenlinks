edeb6673597ac4374aec536d4b5119bf77ed4398
Nathan LeClaire <nathan.leclaire@gmail.com>
2013-11-14 20:00:31 -0500

    revise urls to be congruent with go serving things

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,7 +6,7 @@ angular.module('myApp.directives', []).
   directive('testUrlStatus', function() {
   	return {
   		restrict: 'E',
-  		templateUrl: '/app/partials/test_url.html',
+  		templateUrl: '/partials/test_url.html',
       // set scope so that we can have isolate scope on these
       // directives (they are ng-repeated)
       // scope: {
@@ -18,7 +18,7 @@ angular.module('myApp.directives', []).
   .directive('makingCallSpinner', function() {
     return {
       restrict: 'E',
-      templateUrl: '/app/partials/call_spinner.html',
+      templateUrl: '/partials/call_spinner.html',
       transclude: true
     }
   });
