3cfa49ace650d680f6816a9ffad8b2d27df8943e
Nathan LeClaire <nathan.leclaire@gmail.com>
2013-11-15 19:27:57 -0500

    A bunch of stuff in one YOLO commit.  Proper JSON response with the relevant links and such now

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,20 +3,21 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MainCtrl', function($scope, $http) {
-  	$scope.runningTest = false;
-  	$scope.done_scraping_original_url = false;
-  	$scope.startRunningTest = function() {
-  		$scope.runningTest = true;
-  		console.log($scope.url_to_scrape);
-  		$http.get('/slurp', {
-			params: {
-				url_to_scrape: $scope.url_to_scrape
-			}
-  		})
-  		.success(function(data) {
-  			$scope.done_scraping_original_url = true;
-  			$scope.externalWebSiteHTML = data;
-  		});
-  	};
-  });
+controller('MainCtrl', function($scope, $http) {
+    $scope.runningTest = false;
+    $scope.done_scraping_original_url = false;
+    $scope.startRunningTest = function() {
+        $scope.runningTest = true;
+        console.log($scope.url_to_scrape);
+        $http.get('/slurp', {
+            params: {
+                url_to_scrape: $scope.url_to_scrape
+            }
+        })
+            .success(function(data) {
+                console.log(data);
+                $scope.done_scraping_original_url = true;
+                $scope.externalWebSiteJSON = angular.toJson(data, true);
+            });
+    };
+});
--- app/server/main.go
+++ app/server/main.go
@@ -1,25 +1,59 @@
-package main;
+package main
 
 import (
+	"encoding/json"
+	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
-	"io/ioutil"
+	"regexp"
 )
+
+type ParsedResponse struct {
+	Success bool     `json:"success"`
+	Links   []string `json:"links"`
+}
 
 func slurpHandler(w http.ResponseWriter, r *http.Request) {
 	url_to_scrape := r.URL.Query().Get("url_to_scrape")
 	log.Print("query raw", url_to_scrape)
 
-	urlToParseResponse, err := http.Get(url_to_scrape);
-	if err != nil {
-		log.Print("err in slurp call: ", err, " url: ", url_to_scrape)
-	}
-	rawResponse, err := ioutil.ReadAll(urlToParseResponse.Body)
-	if err != nil {
-		log.Print("err in slurp call: ", err, " url: ", url_to_scrape)
+	var doc *goquery.Document
+	var e error
+
+	links := []string{}
+
+	if doc, e = goquery.NewDocument(url_to_scrape); e != nil {
+		log.Print("error querying for document: ", url_to_scrape, "err : ", e)
 	}
 
-	w.Write(rawResponse)
+	crossDomainRegex, err := regexp.Compile(`^http`)
+	if err != nil {
+		log.Printf("issue compiling regular expression to validate cross domain URLs")
+	}
+
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists != true {
+			log.Print("href does not exist for: ", s)
+		} else {
+			if crossDomainRegex.Match([]byte(href)) {
+				links = append(links, href)
+			}
+		}
+	})
+
+	parsedResponse := &ParsedResponse{true, links}
+	parsedResponseJSON, err := json.Marshal(parsedResponse)
+
+	if err != nil {
+		parsedResponse := &ParsedResponse{false, nil}
+		parsedResponseJSON, err := json.Marshal(parsedResponse)
+		if err != nil {
+			log.Print("something went really weird in attempt to marshal a fail json ", err, parsedResponseJSON)
+		}
+	}
+
+	w.Write(parsedResponseJSON)
 
 }
 
@@ -28,7 +62,7 @@ func slurpHandler(w http.ResponseWriter, r *http.Request) {
 //                (pass it as a string[] in a struct, marshalled to JSON)
 
 func main() {
-	http.HandleFunc("/slurp", slurpHandler);
+	http.HandleFunc("/slurp", slurpHandler)
 	http.Handle("/", http.FileServer(http.Dir("..")))
 	http.Handle("/css/", http.FileServer(http.Dir("..")))
 	http.Handle("/img/", http.FileServer(http.Dir("..")))
