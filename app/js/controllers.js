0f0badff4ec4f2996d0096290be19f5f955850bf
Nathan LeClaire <nathanleclaire@gmail.com>
2013-11-28 02:13:51 -0500

    just don't make the slurp call if the URL is bogus

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,41 +21,45 @@ controller('MainCtrl', function($scope, $http) {
 		$scope.done_scraping_original_url = false;
         $scope.linksInfo = [];
         $scope.parseOriginalUrlStatus = 'calling';
-        $http.get('/slurp', {
-            params: {
-                url_to_scrape: $scope.url_to_scrape
-            }
-        })
-            .success(function(data) {
-                console.log(data);
-                if (!data.success) {
-                    console.log('no success');
-                    $scope.parseOriginalUrlStatus = 'invalid';
-                } else {
-                    $scope.parseOriginalUrlStatus = 'waiting';
-                    $scope.done_scraping_original_url = true;
-                    $scope.externalWebSiteJSON = angular.toJson(data, true);
-                    if (data.success) {
-                        var links = data.links;
-						if (links.length > 1) {
-							var linksInfo = [];
-							for (var i = 0; i < links.length; i++) {
-								linksInfo.push({
-									href: links[i],
-									checkResult: undefined,
-								});
+		if (!$scope.linkCheckForm.url_to_scrape.$error.pattern) {
+			$http.get('/slurp', {
+				params: {
+					url_to_scrape: $scope.url_to_scrape
+				}
+			})
+				.success(function(data) {
+					console.log(data);
+					if (!data.success) {
+						console.log('no success');
+						$scope.parseOriginalUrlStatus = 'invalid';
+					} else {
+						$scope.parseOriginalUrlStatus = 'waiting';
+						$scope.done_scraping_original_url = true;
+						$scope.externalWebSiteJSON = angular.toJson(data, true);
+						if (data.success) {
+							var links = data.links;
+							if (links.length > 1) {
+								var linksInfo = [];
+								for (var i = 0; i < links.length; i++) {
+									linksInfo.push({
+										href: links[i],
+										checkResult: undefined,
+									});
+								}
+								$scope.retrieved_urls = linksInfo;
+								for (var i = 0; i < linksInfo.length; i++) {
+									$scope.checkUrl(i);
+								}
+							} else {
+								$scope.parseOriginalUrlStatus = 'invalid';	
 							}
-							$scope.retrieved_urls = linksInfo;
-							for (var i = 0; i < linksInfo.length; i++) {
-								$scope.checkUrl(i);
-							}
-						} else {
-							$scope.parseOriginalUrlStatus = 'invalid';	
 						}
-                    }
-                }
+					}
 
-            });
+				});
+		} else {
+			$scope.parseOriginalUrlStatus = 'invalid';
+		}
     };
     $scope.checkUrl = function(url_to_check_index) {
         console.log("checking ", $scope.retrieved_urls[url_to_check_index].href);
