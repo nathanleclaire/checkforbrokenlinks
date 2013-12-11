1b56bfcba0c4fc5866b7f462c620327f99325205
Nathan LeClaire <nathanleclaire@gmail.com>
2013-12-10 19:03:58 -0500

    angular recaptcha client side work

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('checkForBrokenLinksApp', [
     'ngRoute',
+    'vcRecaptcha',
     'checkForBrokenLinksApp.filters',
     'checkForBrokenLinksApp.services',
     'checkForBrokenLinksApp.directives',
