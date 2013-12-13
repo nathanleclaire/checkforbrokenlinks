8fd86725a97176e1c0bada76533a8b5f9d1ac209
Nathan LeClaire <nathanleclaire@gmail.com>
2013-12-12 19:49:48 -0500

    finally make animations work.. finally

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('checkForBrokenLinksApp', [
     'ngRoute',
+    'ngAnimate',
     'vcRecaptcha',
     'checkForBrokenLinksApp.filters',
     'checkForBrokenLinksApp.services',
